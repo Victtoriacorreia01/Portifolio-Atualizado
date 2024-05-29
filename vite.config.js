b566d6622bb6d1fccf7d89ab7c3f932026dad283
victtoriacorreia <victtoria.correa01@gmail.com>
2024-05-29 15:39:11 -0300

    commit inicial

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
-export default {
-  build: {
-    rollupOptions: {
-      external: ['aos/dist/aos.css', '@fortawesome/react-fontawesome', '@fortawesome/free-brands-svg-icons', '@fortawesome/free-solid-svg-icons']
-    }
-  }
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/portvic/",
+  plugins: [react()],
+})
